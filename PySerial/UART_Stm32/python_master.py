c760caa067d960088e9ba8f9f59a571b2a36da1a
dusktopia <lehuynhduc160699@gmail,com>
2023-02-25 21:01:50 +0700

    Modify pyserial

--- PySerial/UART_Stm32/python_master.py
+++ PySerial/UART_Stm32/python_master.py
@@ -1,20 +1,20 @@
 import serial
 import time
 
-master = serial.Serial(port='COM3',
-                       baudrate=9600,
-                       bytesize=serial.EIGHTBITS,
-                       parity=serial.PARITY_NONE,
-                       stopbits=serial.STOPBITS_ONE,
-                       timeout=2)
+# * COM Port configuration
 
-led_on = bytes('LED ON\n', encoding='ascii')
-led_off = bytes('LED OFF\n', encoding='ascii')
+vserial = serial.Serial(port='COM10',
+                        baudrate=9600,
+                        bytesize=serial.EIGHTBITS,
+                        parity=serial.PARITY_NONE,
+                        stopbits=serial.STOPBITS_ONE,
+                        timeout=2)
 
-for i in range(3):
-    master.write(led_on)
-    time.sleep(3)
-    master.write(led_off)
-    time.sleep(3)
+while True:
+    buffer = vserial.in_waiting
+    msg_from_terminal = vserial.read(buffer)
+    if buffer:
+        print(msg_from_terminal.decode())
+        break
 
-master.close()
+vserial.close()
